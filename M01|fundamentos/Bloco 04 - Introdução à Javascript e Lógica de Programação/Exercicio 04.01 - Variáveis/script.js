f633c125a90493958e5a39d26e74e372f6331f01
marianagondin <marianagondin@gmail.com>
2021-12-22 01:47:27 -0300

    Resolvendo errinhos no README e resolvendo primeiro exercicio do bloco 4

---
+++ M01|fundamentos/Bloco 04 - Introdução à Javascript e Lógica de Programação/Exercicio 04.01 - Variáveis/script.js
@@ -0,0 +1,29 @@
+// Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+
+const myName = "Mariana";
+
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+
+const birthCity = "Santa Helena de Goiás";
+
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+
+var birthYear = 2000;
+
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+
+birthYear = 2030;
+console.log(birthYear)
+
+// Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 
+
+birthCity = "Goiânia";
+console.log(birthCity)
+
+// Resposta a pergunta: Porque as variáveis const não podem ser atualizadas nem declaradas novamente.
