296251fd187eaa170c84624893304e57a3e75807
marianagondin <marianagondin@gmail.com>
2021-12-23 02:04:46 -0300

    Praticando switch e case

---
+++ M01|fundamentos/Bloco 04 - Introdução à Javascript e Lógica de Programação/Exercicio 04.01 - Switch e Case/scripts.js
@@ -0,0 +1,18 @@
+var pessoaCandidata;
+
+//Testando o exercício com as possibilidades:
+pessoaCandidata = "aprovada";
+
+switch (pessoaCandidata) {
+    case "aprovada": 
+        console.log("Parabéns, você foi aprovada(o)!");
+      break;
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+    case "reprovada":
+        console.log("Você foi reprovada(o)");
+        break;
+    default:
+        console.log("Não se aplica");
+}
