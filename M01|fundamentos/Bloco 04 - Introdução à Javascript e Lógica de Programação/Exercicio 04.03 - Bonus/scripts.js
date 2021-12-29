a24646f5b988bf450b8d9440dec316f0eb353be0
marianagondin <marianagondin@gmail.com>
2021-12-29 12:09:51 -0300

    Resolvendo exercicio dos asteriscos

---
+++ M01|fundamentos/Bloco 04 - Introdução à Javascript e Lógica de Programação/Exercicio 04.03 - Bonus/scripts.js
@@ -0,0 +1,11 @@
+let n = 5;
+let asterisco = "*";
+let stringAsterisco = "";
+
+for (let i = 0; i < n; i += 1) {
+    stringAsterisco = stringAsterisco + asterisco;
+}
+
+for(let index = 0; index < n; index += 1) {
+    console.log(stringAsterisco)
+}
