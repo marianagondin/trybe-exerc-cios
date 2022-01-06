12b343a23d441799ca5239e56e14eb40703e65f2
marianagondin <marianagondin@gmail.com>
2022-01-06 15:05:36 -0300

    Gabarito da trybe

--- M01|fundamentos/Bloco 05 - JavaScript - Dom, Eventos e Web Storage/Exercicio 05.02 - Parte III - Removendo elementos/script.js
+++ M01|fundamentos/Bloco 05 - JavaScript - Dom, Eventos e Web Storage/Exercicio 05.02 - Parte III - Removendo elementos/script.js
@@ -40,3 +40,15 @@ exceto pai , elementoOndeVoceEsta e
 primeiroFilhoDoFilho.
 */
 
+// Gabarito da Trybe:
+const pai = document.getElementById('pai');
+
+    for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+      const currentChildren = pai.childNodes[index];
+      if (currentChildren.id !== 'elementoOndeVoceEsta') {
+        currentChildren.remove();
+      }
+    }
+
+    const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+    segundoEUltimoFilhoDoFilho.remove();
